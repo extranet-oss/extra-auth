2b9b760c05672613ea5df573aff51e5d5d122cb5
Jordan Demaison <Jordan.Demaison@gmail.com>
2018-06-07 03:24:25 +0200

    Complete rewrite using koa + OIDC provider integration
    - koa integrates nicer than express with oidc-provider

--- app.js
+++ app.js
@@ -1,36 +1,95 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const Koa = require('koa');
+const morgan = require('koa-morgan');
+const conditional = require('koa-conditional-get');
+const etag = require('koa-etag');
+const path = require('path');
+const hbs = require('koa-hbs');
+const serve = require('koa-static');
+const Router = require('koa-router');
+const mount = require('koa-mount');
 
-var app = express();
-app.disable('x-powered-by');
+// Create app
+const app = new Koa();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// Trust proxy
+app.proxy = true;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use('/static', express.static(path.join(__dirname, 'public')));
+// Request logger
+app.use(morgan('dev'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Conditionnal GET for saving bandwidth
+app.use(conditional());
+app.use(etag());
+
+// View engine setup (shared with sub-app)
+const view_engine = hbs.middleware({
+  viewPath: path.join(__dirname, 'views'),
+  defaultLayout: 'layout'
+});
+app.use(view_engine);
+
+// Public directory setup
+app.use(serve(path.join(__dirname, 'public'), {
+  maxage: 0
+}));
+
+// Error handler
+const error_handler = async (ctx, out, error) => {
+  ctx.state = {
+    layout: 'layout',
+    title: error.message,
+    message: error.message,
+    error: ctx.app.env === 'development' ? `${error.stack}\n\n${JSON.stringify(error, null, 4)}` : {}
+  };
+
+  await ctx.render('error');
+};
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (error) {
+    await error_handler(ctx, {}, error);
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = res.locals.title = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Create default routes
+const router = new Router();
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(router.routes())
+.use(router.allowedMethods());
+
+// OIDC Provider setup
+const Provider = require('oidc-provider');
+
+const oidc = new Provider('https://login.dev.tryextra.net/', {
+  routes: {
+    authorization: '/oauth/authorize',
+    certificates: '/discovery/certs',
+    revocation: '/oauth/revoke',
+    token: '/oauth/token',
+    userinfo: '/openid/userinfo',
+  },
+  features: {
+    devInteractions: false
+  },
+  renderError: async (ctx, out, error) => {
+    switch (out.error) {
+
+      case 'invalid_request':
+        error.message = 'Invalid Request';
+        break;
+
+      default:
+        error.message = out.error_description;
+    }
+    await error_handler(ctx, out, error);
+  }
+});
+
+oidc.initialize({ }).then(() => {
+  oidc.use(view_engine);
+  app.use(mount(oidc.app));
 });
 
 module.exports = app;
