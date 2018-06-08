fe5feb42dc0856de5f60c9017edc4a5d9edb3a0e
Jordan Demaison <Jordan.Demaison@gmail.com>
2018-06-08 05:08:41 +0200

    Added basic azuread signin flow
    - new features comparing to previous logins: directly opens epitech login to reduce steps, using form_post

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const path = require('path');
 const hbs = require('koa-hbs');
 const serve = require('koa-static');
 const statuses = require('statuses');
+const session = require('koa-session');
+const bodyparser = require('koa-body');
+const passport = require('koa-passport');
+const { OIDCStrategy } = require('passport-azure-ad');
 const Router = require('koa-router');
 const mount = require('koa-mount');
+
+const config = require('./config.json');
 
 // Create app
 const app = new Koa();
@@ -56,6 +62,41 @@ app.use(async (ctx, next) => {
   }
 });
 
+// session setup
+app.use(session({ signed: false }, app));
+
+// parse body
+app.use(bodyparser({ patchNode: true }));
+
+// setup passport strategies
+app.use(passport.initialize())
+app.use(passport.session())
+
+function external_scheme() {
+  return config.host.startsWith('127.0.0.1') ? 'http' : 'https';
+}
+function external_url(path) {
+  return `${external_scheme()}://${config.host}${path}`;
+}
+
+passport.use(new OIDCStrategy({
+    identityMetadata: `https://login.microsoftonline.com/${config.azuread.tenantID}/.well-known/openid-configuration`,
+    clientID: config.azuread.clientID,
+    responseType: 'id_token',
+    responseMode: 'form_post',
+    redirectUrl: external_url('/interaction/azuread'),
+    allowHttpForRedirectUrl: external_scheme() == 'http',
+    passReqToCallback: false
+  },
+  function(iss, sub, profile, accessToken, refreshToken, done) {
+    if (!profile.oid) {
+      return done(new Error('No oid found'), null);
+    }
+    console.log("azuread:", {iss, sub, profile, accessToken, refreshToken})
+    done(null, profile);
+  }
+));
+
 // Create default routes
 const router = new Router();
 
@@ -65,13 +106,16 @@ app.use(router.routes())
 // OIDC Provider setup
 const Provider = require('oidc-provider');
 
-const oidc = new Provider('https://login.dev.tryextra.net/', {
+const oidc = new Provider(external_url(''), {
   routes: {
     authorization: '/oauth/authorize',
     certificates: '/discovery/certs',
     revocation: '/oauth/revoke',
     token: '/oauth/token',
     userinfo: '/openid/userinfo',
+  },
+  async interactionUrl(ctx, interaction) {
+    return `/interaction/?request_id=${ctx.oidc.uuid}`;
   },
   features: {
     devInteractions: false
@@ -88,6 +132,56 @@ const oidc = new Provider('https://login.dev.tryextra.net/', {
 
 oidc.initialize({ }).then(() => {
   oidc.use(view_engine);
+
+  async function checkFlowSession(ctx, next) {
+    ctx.assert(ctx.cookies.get(oidc.cookieName('interaction')), 401, 'Session expired');
+    const details = await oidc.interactionDetails(ctx.req);
+    ctx.assert(details.uuid, 401, 'Session expired');
+    ctx.state.details = details;
+    await next();
+  }
+
+  router.get('/interaction/',
+    checkFlowSession,
+    async ctx => {
+      await ctx.redirect(`/interaction/signin?request_id=${ctx.state.details.uuid}`);
+    });
+
+  router.get('/interaction/signin',
+    checkFlowSession,
+    async ctx => {
+      ctx.state.title = ctx.state.details.interaction.reason_description;
+
+      console.log(ctx.state.details)
+      await ctx.render('login');
+    });
+
+  router.get('/interaction/azuread',
+    checkFlowSession,
+    async (ctx, next) => {
+      await passport.authenticate('azuread-openidconnect', {
+        failureRedirect: `/interaction/signin?request_id=${ctx.state.details.uuid}`,
+        session: false,
+        domain_hint: config.azuread.tenantDomain
+      })(ctx, next);
+    })
+  .post('/interaction/azuread',
+    checkFlowSession,
+    async (ctx, next) => {
+      await passport.authenticate('azuread-openidconnect', {
+        failureRedirect: `/interaction/signin?request_id=${ctx.state.details.uuid}`,
+        session: false
+      })(ctx, next);
+    },
+    async ctx => {
+      await oidc.interactionFinished(ctx.req, ctx.res, {
+        login: {
+          account: ctx.state.user.oid
+        },
+        consent: {}
+      });
+    })
+
   app.use(mount(oidc.app));
 });
 
