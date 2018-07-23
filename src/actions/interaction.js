d9fb2f47154812a46f7004c4ec1d51db76a3e8c2
Jordan Demaison <Jordan.Demaison@gmail.com>
2018-07-23 14:53:45 +0200

    Trusted clients do not need any consent

--- src/actions/interaction.js
+++ src/actions/interaction.js
@@ -10,15 +10,28 @@ module.exports = function (router, oidc, client, config) {
   const clients = client.service('clients');
 
   async function login_prompt(ctx) {
-    ctx.state.title = ctx.state.details.interaction.reason_description;
-
-    if (ctx.state.client.trusted && ctx.state.details.params.bypass_signin !== undefined)
+    if (ctx.state.client.trusted && ctx.state.details.params.bypass_signin !== undefined) {
+      debug(`Sign-in screen bypass accepted`);
       await ctx.redirect(`/interaction/azuread?request_id=${ctx.state.details.uuid}`);
-    else
-      await ctx.render('login');
+      return;
+    }
+
+    ctx.state.title = 'Please Sign-in to continue.';
+    if (ctx.state.details.accountId)
+      ctx.state.title = 'Please Sign-in again to continue.';
+
+    await ctx.render('login');
   }
 
   async function consent_prompt(ctx) {
+    if (ctx.state.client.trusted) {
+      debug(`Client is trusted, ignoring consent`);
+      await oidc.interactionFinished(ctx.req, ctx.res, {
+        consent: {}
+      });
+      return;
+    }
+
     ctx.state.title = ctx.state.details.interaction.reason_description;
     ctx.state.scopes = ctx.state.details.params.scope.split(' ');
 
@@ -28,13 +41,14 @@ module.exports = function (router, oidc, client, config) {
   router.get(dest,
     checkSession,
     async function interaction(ctx) {
-      debug('Interaction start');
+      debug('Interaction start', ctx.state.details.interaction.reason);
 
       if (!ctx.state.details.meta) {
         ctx.state.details.meta = {
           done: []
         };
         ctx.state.details.save();
+        debug(`Initialized interaction meta data`);
       }
 
       ctx.state.client = await clients.get(ctx.state.details.params.client_id);
@@ -47,6 +61,7 @@ module.exports = function (router, oidc, client, config) {
           !ctx.state.details.meta.done.includes('login')))
         prompt = 'login';
 
+      debug(`Selecting ${prompt} prompt`);
       switch (prompt) {
         case 'login':
           await login_prompt(ctx);
